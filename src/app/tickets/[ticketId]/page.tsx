a93f11b78a1031b068990cd1560e71024b861e55
Lilian Burdianov <lilian.burdianov@gmail.com>
2024-12-28 11:54:10 +0400

    add Link navigation

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,10 +1,25 @@
 import { use } from "react";
 
+import { initialTickets } from "@/data";
+
 interface TicketPageProps {
   params: Promise<{ ticketId: string }>;
 }
 
 export default function TicketPage({ params }: TicketPageProps) {
   const { ticketId } = use(params);
-  return <h2 className="text-lg">Ticket Page {ticketId}</h2>;
+  const ticket = initialTickets.find(
+    (ticket) => ticket.id === Number(ticketId)
+  );
+
+  if (!ticket) {
+    return <div>Ticket not found</div>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <p className="text-sm">{ticket.content}</p>
+    </div>
+  );
 }
