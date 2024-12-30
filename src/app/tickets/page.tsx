f64c900cf9fc1141a7c6cd4fff22658fee78eee6
Lilian Burdianov <lilian.burdianov@gmail.com>
2024-12-30 10:16:11 +0400

    styling

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -11,16 +11,28 @@ const TICKET_ICONS = {
 
 export default function TicketsPage() {
   return (
-    <div>
-      {initialTickets.map((ticket) => (
-        <div key={ticket.id}>
-          <div>{TICKET_ICONS[ticket.status]}</div>
-          <h2 className="text-lg">{ticket.title}</h2>
-          <Link href={ticketPath(ticket.id)} className="text-sm underline">
-            View
-          </Link>
-        </div>
-      ))}
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
+        <p className="text-sm text-muted-foreground">
+          All your tickets at one place
+        </p>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center gap-y-4">
+        {initialTickets.map((ticket) => (
+          <div
+            key={ticket.id}
+            className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
+          >
+            <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+            <p className="text-sm text-slate-500 truncate">{ticket.content}</p>
+            <Link href={ticketPath(ticket.id)} className="text-sm underline">
+              View
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
