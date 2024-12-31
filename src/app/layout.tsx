95b78b636a4da5fe512669e8629917997c77f168
Lilian Burdianov <lilian.burdianov@gmail.com>
2025-01-01 03:14:58 +0400

    install and use Button component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 import "./globals.css";
 
+import { Button, buttonVariants } from "@/components/ui/button";
 import { homePath, ticketsPath } from "@/paths";
 
 const geistSans = Geist({
@@ -33,12 +34,15 @@ export default function RootLayout({
       >
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-10 bg-background/95 backdrop-blur w-full flex justify-between py-2.5 px-5 border-b">
           <div>
-            <Link href={homePath()} className="text-lg font-bold">
-              Home
-            </Link>
+            <Button asChild variant="outline">
+              <Link href={homePath()}>Home</Link>
+            </Button>
           </div>
           <div>
-            <Link href={ticketsPath()} className="text-sm underline">
+            <Link
+              href={ticketsPath()}
+              className={buttonVariants({ variant: "outline" })}
+            >
               Tickets
             </Link>
           </div>
