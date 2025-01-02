a94bec95cadf0b65fb1628ab0c7eab4298d10b27
Lilian Burdianov <lilian.burdianov@gmail.com>
2025-01-02 20:58:16 +0400

    refactor heading component

--- src/components/custom/heading.tsx
+++ src/components/custom/heading.tsx
@@ -2,15 +2,15 @@ import { Separator } from "../ui/separator";
 
 interface HeadingProps {
   title: string;
-  description: string;
+  description?: string;
 }
 
 export default function Heading({ title, description }: HeadingProps) {
   return (
     <>
-      <div>
+      <div className="px-8">
         <h2 className="text-3xl font-bold tracking-tight">{title}</h2>
-        <p className="text-sm text-muted-foreground">{description}</p>
+        {description && <p className="text-muted-foreground">{description}</p>}
       </div>
 
       <Separator />
