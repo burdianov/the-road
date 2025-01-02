a66a67bc115d0b33932795ce6bd42175116044d9
Lilian Burdianov <lilian.burdianov@gmail.com>
2025-01-02 20:55:17 +0400

    create custom heading component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,6 +1,7 @@
 import { CircleCheck, FileText, Pencil } from "lucide-react";
 import Link from "next/link";
 
+import Heading from "@/components/custom/heading";
 import {
   Card,
   CardContent,
@@ -8,7 +9,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
@@ -21,14 +21,10 @@ const TICKET_ICONS = {
 export default function TicketsPage() {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
-      <div>
-        <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
-        <p className="text-sm text-muted-foreground">
-          All your tickets at one place
-        </p>
-      </div>
-
-      <Separator />
+      <Heading
+        title="Tickets Page"
+        description="All your tickets at one place"
+      />
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
         {initialTickets.map((ticket) => (
