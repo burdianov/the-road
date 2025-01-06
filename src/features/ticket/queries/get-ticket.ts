5a3a815b0fea68c9120fa84f29cc7400c62070c1
Lilian Burdianov <lilian.burdianov@gmail.com>
2025-01-06 13:59:07 +0400

    refactoring

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -3,13 +3,13 @@ import { Ticket } from "../types";
 import { initialTickets } from "@/data";
 
 export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
-    await new Promise((resolve) => {
-        setTimeout(resolve, 2000);
-    });
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
 
-    const maybeTicket = initialTickets.find((ticket) => ticket.id === ticketId);
+  const maybeTicket = initialTickets.find((ticket) => ticket.id === ticketId);
 
-    return new Promise((resolve) => {
-        resolve(maybeTicket || null);
-    });
+  return new Promise((resolve) => {
+    resolve(maybeTicket || null);
+  });
 };
--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -3,11 +3,11 @@ import { Ticket } from "../types";
 import { initialTickets } from "@/data";
 
 export const getTickets = async (): Promise<Ticket[]> => {
-    await new Promise((resolve) => {
-        setTimeout(resolve, 2000);
-    });
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
 
-    return new Promise((resolve) => {
-        resolve(initialTickets);
-    });
+  return new Promise((resolve) => {
+    resolve(initialTickets);
+  });
 };
